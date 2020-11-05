176571e2568c5385016dd025fd1ef2c6e4d7f0da
Pavel4444 <qwert79@seznam.cz>
2020-11-05 12:37:38 +0100

    added swagger inside backend

--- src/main/java/com/example/demo/Controllers.java
+++ src/main/java/com/example/demo/Controllers.java
@@ -16,41 +16,58 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.CrudTable.Employee;
 import com.example.demo.CrudTable.EmployeeService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@RequestMapping("/api")
+
 public class Controllers {
+	
 	
 	@Autowired
 	  private EmployeeService courseManagementService;
 	
-  @GetMapping("/api/hello")
+  @GetMapping("/hello")
+  @ApiOperation(value = "Basic api string test")
+  
   public String hello() {
       return "Hello world";
   }
   
-  @GetMapping("/api/employees2/{id}")
+  @GetMapping("/employees2/{id}")
+  @ApiOperation(value = "Find employees by ID",
+  notes = "Provide ID in order to look up specific employee"
+		  )
 	public Employee getEmployee(@PathVariable Integer id) {
 		return courseManagementService.findById(id);
 	}
   
-	@GetMapping("/api/employees2")
+	@GetMapping("/employees2")
+	  @ApiOperation(value = "Find all employees"
+			  )
 	public List<Employee> getAllCourses() {
 		return courseManagementService.findAll();
 	}
 	
-	  @PutMapping("/api/employees2/{id}")
+	  @PutMapping("/employees2/{id}")
+	  @ApiOperation(value = "Edit existing employee by ID"
+			  )
 	  public ResponseEntity<Employee> updateCourse(@PathVariable Integer id,
 	      @RequestBody Employee employee) {
 		  Employee employeeUpdated = courseManagementService.save(employee);
 	    return new ResponseEntity<Employee>(employeeUpdated, HttpStatus.OK);
 	  }
 	  
-	  @PostMapping("/api/employees2")
+	  @PostMapping("/employees2")
+	  @ApiOperation(value = "Add new employee by ID"
+			  )
 	  public ResponseEntity<Void> createEmployee(@RequestBody Employee employee) {
 		  Employee createdEmployee = courseManagementService.save(employee);
 	    // Location
@@ -61,7 +78,9 @@ public class Controllers {
 	    return ResponseEntity.created(uri).build();
 	  }
 	
-	@DeleteMapping("/api/employees2/{id}")
+	@DeleteMapping("/employees2/{id}")
+	@ApiOperation(value = "Delete employee by ID"
+			  )
 	  public ResponseEntity<Void> deleteCourse(@PathVariable Integer id) {
 	    Employee employee = courseManagementService.deleteById(id);
 	    if (employee != null) {
@@ -71,7 +90,10 @@ public class Controllers {
 	  }
 	
 	
-    @GetMapping("/api/corona1")
+    @GetMapping("/corona1")
+    @ApiOperation(value = "Get daily coronavirus data on each country",
+    notes = "Data are provided via https://covid19.mathdro.id/api/daily"
+			  )
 	public static String getDailyData() throws IOException{
 
 		HttpURLConnection connection = (HttpURLConnection) new URL("https://covid19.mathdro.id/api/daily").openConnection();
@@ -95,7 +117,10 @@ public class Controllers {
 
 
 	    
-	    @GetMapping("/api/corona2")
+	    @GetMapping("/corona2")
+	    @ApiOperation(value = "Get summary coronavirus data on each country and total",
+	    notes = "Data are provided via https://api.covid19api.com/summary"
+				  )
 		public static String getCoronaData() throws IOException{
 
 			HttpURLConnection connection = (HttpURLConnection) new URL("https://api.covid19api.com/summary").openConnection();
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,57 @@
 package com.example.demo;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+
+	@Bean
+	
+	public Docket swaggerConfiguration() {
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()				
+				.apis(RequestHandlerSelectors.basePackage("com.example"))
+				.build()
+				.apiInfo(apiDetails());
+				
+		
+	}
+	
+	private ApiInfo apiDetails () {
+		
+		return new ApiInfo(
+				"DataTables API Info",
+				"Spring boot backend API documentation for DataTables project",
+				"1.0",
+				"Free",
+				new springfox.documentation.service.Contact("Pavel Pivovarcik", "https://github.com/Pavel4444/DataTables-1.0", "ppivov@gmail.com"),
+				"API License",
+				"https://github.com/Pavel4444/DataTables-1.0",
+				Collections.emptyList());		
+				
+				
+		
+	}
+	
+
+	
 
 }
